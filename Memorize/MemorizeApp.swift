b3b19145d35e6b895add1070537f6f3a8409659f
momo <jiang.wenbo@immomo.com>
2021-02-23 18:27:57 +0800

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by momo on 2021/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
