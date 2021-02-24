2f991df83a6fc552cb0d237b0da52659401a5c58
momo <jiang.wenbo@immomo.com>
2021-02-24 15:14:24 +0800

    first lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    //read only computed property
+    //opaque types, the reverse of generic types
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack {
+            ForEach(0..<4) {index in
+                ZStack {
+                    CardView(isFaceUp: false)
+                }
+            }
+        }
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3.0)
+                Text("👻")
+            }else{
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
     }
 }
 
